9fb96499d2cfff291a6b3337b95b6d4e859e3219
Michael LINHARDT <mlinhard@student.42.fr>
2015-12-23 20:21:30 +0100

    .

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 #    By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/12/23 18:45:20 by mlinhard          #+#    #+#              #
-#    Updated: 2015/12/23 20:20:57 by mlinhard         ###   ########.fr        #
+#    Updated: 2015/12/23 20:21:29 by mlinhard         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
@@ -14,7 +14,7 @@
 
 # Include colors
 echo "[ W8 ] include colors var"
-$(curl -fsSL https://raw.githubusercontent.com/nesthub/workspace/master/config/zsh/colors)"
+curl -fsSL https://raw.githubusercontent.com/nesthub/workspace/master/config/zsh/colors
 # 1. Clone ~/42/workspace
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/nesthub/workspace/master/sh/workspace/clone.sh)"
 echo "ok"
