41a35dac374bb785ca61596a0467797e82878e6a
Michael LINHARDT <mlinhard@student.42.fr>
2015-12-23 19:58:48 +0100

    .

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@
 #    By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/12/23 18:45:20 by mlinhard          #+#    #+#              #
-#    Updated: 2015/12/23 19:56:01 by mlinhard         ###   ########.fr        #
+#    Updated: 2015/12/23 19:58:46 by mlinhard         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
-#sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/nesthub/workspace/master/install.sh)"
+# sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/nesthub/workspace/master/install.sh)"
 echo "ok"
 exit 0
