aa8f4d6a29c8850ad1c78c23a4fafd9737c20381
Michael LINHARDT <mlinhard@student.42.fr>
2015-12-23 19:54:05 +0100

    prepare install script

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@
 #    By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/12/23 18:45:20 by mlinhard          #+#    #+#              #
-#    Updated: 2015/12/23 18:45:28 by mlinhard         ###   ########.fr        #
+#    Updated: 2015/12/23 19:53:44 by mlinhard         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
-
+#sudo sh -c "$(curl -fsSL https://raw.github.com/nesthub/workspace/install.sh)"
 exit 0
