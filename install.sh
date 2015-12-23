1ad7cd6dbc19bffadaaa2ac64261ce02b8f0d287
Michael LINHARDT <mlinhard@student.42.fr>
2015-12-23 20:18:33 +0100

    .

--- install.sh
+++ install.sh
@@ -6,12 +6,15 @@
 #    By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/12/23 18:45:20 by mlinhard          #+#    #+#              #
-#    Updated: 2015/12/23 20:12:18 by mlinhard         ###   ########.fr        #
+#    Updated: 2015/12/23 20:17:10 by mlinhard         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
 # sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/nesthub/workspace/master/install.sh)"
 
+# Include colors
+echo "[ W8 ] include colors var"
+source "$(curl -fsSL https://raw/githubusercontent.com/nesthub/workspace/master/config/zsh/colors)"
 # 1. Clone ~/42/workspace
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/nesthub/workspace/master/sh/workspace/clone.sh)"
 echo "ok"
--- sh/workspace/clone.sh
+++ sh/workspace/clone.sh
@@ -6,16 +6,16 @@
 #    By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/12/23 20:05:34 by mlinhard          #+#    #+#              #
-#    Updated: 2015/12/23 20:13:08 by mlinhard         ###   ########.fr        #
+#    Updated: 2015/12/23 20:18:29 by mlinhard         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
 
-echo "erase and re-clone ~/42/workspace from github [y/n]"
+echo $CW8 $CYE"erase and re-clone ~/42/workspace from github [y/n]"$CWH
 read -r confirm
 if [ "$confirm" != "y" ]
 then
-	echo "Canceled!"
+	echo $CKO $CRE"Canceled!"$CWH
 	exit 1;
 fi
 echo "go"
