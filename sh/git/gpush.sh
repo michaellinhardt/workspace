faba2684382c235d326f447356b98b481025cf20
Michael LINHARDT <mlinhard@student.42.fr>
2015-12-23 18:32:25 +0100

    split gpush with gstatus

--- sh/git/gpush.sh
+++ sh/git/gpush.sh
@@ -6,10 +6,14 @@
 #    By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/12/23 15:29:16 by mlinhard          #+#    #+#              #
-#    Updated: 2015/12/23 18:28:47 by mlinhard         ###   ########.fr        #
+#    Updated: 2015/12/23 18:31:43 by mlinhard         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
+
+sh ~/42/workspace/sh/git/gstatus.sh
+echo $?
+exit 0
 
 # Affiche le status et controle le retour, exit si ce nest pas un dossier git
 echo $CW8 $CYE"git status -s"$CWH
