84c1018d38ef1dfe3f498151a6c7546cae6a0004
Michael LINHARDT <mlinhard@nestmacbook.local>
2015-12-24 21:31:00 +0100

    first push install-settings.sh

---
+++ sh/workspace/install-settings.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# active/desactive le re-clone du dossier workspace
+install_workspace=0
+
+# active/desactive l'installation de brew
+install_brew=1
+
+# active/desactive l'installation de vim
+install_vim=1
+
+# active/desactive l'installation de vm player
+install_vmplayer=1
+
+# active/desactive l'installation de terminator
+install_terminator=1
