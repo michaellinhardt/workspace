3eebe39439babe7b93523b92aa678fb022fb33a9
Michael LINHARDT <mlinhard@nestmacbook.local>
2015-12-25 00:49:25 +0100

    add var for install and config each app

--- sh/workspace/install-settings.sh
+++ sh/workspace/install-settings.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# active/desactive le re-clone du dossier workspace
+# active/desactive installation
 install_workspace=0
-
-# active/desactive l'installation de brew
 install_brew=1
-
-# active/desactive l'installation de vim
-install_vim=1
-
-# active/desactive l'installation de vm player
 install_vmplayer=1
+install_iterm=1
+install_sublimetext=1
+install_firefox=1
 
-# active/desactive l'installation de terminator
-install_terminator=1
+ #active/desactive configuration
+config_zsh=1
+config_vim=1
+config_iterm=1
+config_sublimetext=1
+config_firefox=1
+
+
