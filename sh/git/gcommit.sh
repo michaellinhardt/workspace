ff02232990be6d6229e3904af78f2a10d5e42043
Michael Linhardt <michael.linhardt@recast.ai>
2017-02-09 09:49:54 +0100

    [ARC] remove 42 header

--- sh/git/gcommit.sh
+++ sh/git/gcommit.sh
@@ -1,14 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    gpush.sh                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2015/12/23 15:29:16 by mlinhard          #+#    #+#              #
-#    Updated: 2017/01/26 10:21:51 by mlinhard         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
 #!/bin/bash
 
 sh ~/42/workspace/sh/git/gclean.sh
@@ -17,8 +6,6 @@ sh ~/42/workspace/sh/git/gsetup.sh
 [[ $? != 0 ]] && exit 1;
 sh ~/42/workspace/sh/git/gstatus.sh
 [[ $? != 0 ]] && exit 1;
-# Si aucun argument transmit on recupere la raison du commit et la liste a push
-# si raison du commit est vide on stop
 # si aucune list a push on push tout
 if [ $# -lt 1 ]
 then
@@ -40,17 +27,8 @@ then
 	fi
 # Si un argument transmit on push tout avec comme commit l'argument transmit
 else
-	# la raison du commit est transmise a lappel du script
 	commit=$*
 	file="--all"
-	# Demande confirmation
-	#echo $CW8 $CYE"Press enter to confirm or send any key to cancel.."$CWH
-	#read -r verify
-	#if [ $verify ]
-	#then
-	#	echo $CKO $CRE"Canceled!"$CWH
-	#	exit 0
-	#fi
 fi
 # Execute l'operation
 echo $CW8 $CYE"git add "$file$CWH
--- sh/git/gstatus.sh
+++ sh/git/gstatus.sh
@@ -1,14 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    gstatus.sh                                         :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2015/12/23 15:29:16 by mlinhard          #+#    #+#              #
-#    Updated: 2015/12/23 18:27:38 by mlinhard         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
 #!/bin/bash
 
 # Affiche le status et controle le retour, exit si ce nest pas un dossier git
