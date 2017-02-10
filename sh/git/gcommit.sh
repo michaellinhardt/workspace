0079f60c92142b11d700a3a8409e712f8b363693
Michael Linhardt <michael.linhardt@recast.ai>
2017-02-10 10:42:15 +0100

    work independently now and dont call each other

--- sh/git/gcommit.sh
+++ sh/git/gcommit.sh
@@ -20,21 +20,11 @@ then
 		echo $CKO $CRE"Canceled!"$CWH
 		exit 0
 	fi
-	# recupere la liste des fichiers a push
-	echo $CW8 $CYE"Enter file list or leave blank for all.."$CWH
-	read -r file
-	if [ ! "$file" ]
-	then
-		file="--all"
-	fi
 # Si un argument transmit on push tout avec comme commit l'argument transmit
 else
 	commit=$*
-	file="--all"
 fi
 # Execute l'operation
-echo $CW8 $CYE"git add "$file$CWH
-git add $file
 echo $CW8 $CYE"git commit -m \""$commit"\""$CWH
 git commit -m "$commit"
 echo $COK $CGR"Done! (not pushed!)"$CWH
--- sh/git/gpush.sh
+++ sh/git/gpush.sh
@@ -5,23 +5,10 @@ sh ~/42/workspace/sh/sys/dstore.sh
 sh ~/42/workspace/sh/git/gsetup.sh
 [[ $? != 0 ]] && exit 1;
 sh ~/42/workspace/sh/git/gstatus.sh
+sh ~/42/workspace/sh/git/glint.sh
+[[ $? != 0 ]] && exit 1;
 # Si aucun argument transmit on push et c'est tout
-if [ $# -lt 1 ]
-then
-  echo $CW8 $CYE"git push"$CWH
-  git push
-  echo $COK $CGR"Done!"$CWH
-  exit 0
-else
-  sh ~/42/workspace/sh/git/glint.sh
-  [[ $? != 0 ]] && exit 1;
-	commit=$*
-  echo $CW8 $CYE"git add --all"
-  git add --all
-  echo $CW8 $CYE"git commit -m \""$commit"\""$CWH
-  git commit -m "$commit"
-  echo $CW8 $CYE"git push"$CWH
-  git push
-  echo $COK $CGR"Done!"$CWH
-  exit 0
-fi
+echo $CW8 $CYE"git push"$CWH
+git push
+echo $COK $CGR"Done!"$CWH
+exit 0
