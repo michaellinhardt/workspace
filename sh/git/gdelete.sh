40cb98aff435d6a4a3e499a40407b342a33aea32
Michael Linhardt <michael.linhardt@recast.ai>
2017-02-28 10:38:13 +0100

    [ADD] gdelete command to remove a git branch localy and on server

---
+++ sh/git/gdelete.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+  echo $CKO $CRE"Merci de specifier une branche..!"$CWH
+	exit 1
+else
+	arg=$*
+fi
+
+# Execute l'operation
+
+echo $CW8 $CYE"git branch -d "$arg $CWH
+git branch -d $arg
+[[ $? != 0 ]] && echo $CKO $CRE"Erreur pendant le delete local!"$CWH && exit 1;
+
+echo $CW8 $CYE"git push origin :"$arg $CWH
+git push origin :$arg
+[[ $? != 0 ]] && echo $CKO $CRE"Erreur pendant le delete distant!"$CWH && exit 1;
+
+echo $COK $CGR"Done!"$CWH
+exit 0
