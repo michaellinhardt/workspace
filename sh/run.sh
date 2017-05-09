e7732b27ae5d815c55c4e5c4748314c1d576cc67
Michael Linhardt <michael.linhardt@recast.ai>
2017-05-09 11:45:59 +0200

    [UPD] modifying run command

--- sh/run.sh
+++ sh/run.sh
@@ -2,5 +2,6 @@
 
 arg=$*
 #nodemon ./src/test/rabbit_sender_receiver/new_task.js --exec ./node_modules/babel-cli/bin/babel-node.js $arg
-babel-node ./src/test/rabbit/rpc_client.js $arg
+#babel-node ./src/test/rabbit/rpc_client.js $arg
+echo "tmp command, type arun to edit this"
 exit 0
