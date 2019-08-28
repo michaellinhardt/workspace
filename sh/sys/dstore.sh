93fc10eee789e0a7680516ed4f7d3fc68dfee744
Michael Linhardt <linhardt.michael@gmail.com>
2019-08-28 12:46:39 +0700

    add: new rule for dstore command

--- sh/sys/dstore.sh
+++ sh/sys/dstore.sh
@@ -2,5 +2,7 @@
 
 echo $CW8"find ./ -name \".DS_Store\" -depth -exec rm {} \;"$CWH
 find ./ -name ".DS_Store" -depth -exec rm {} \;
+echo $CW8"find ./ -name \"_DS_Store\" -depth -exec rm {} \;"$CWH
+find ./ -name "_DS_Store" -depth -exec rm {} \;
 echo $COK "Done!"$CWH
 exit 0;
