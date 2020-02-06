a281437fec8721d299adfc78aa4db999a69a8a45
Michael Linhardt <linhardt.michael@gmail.com>
2020-02-06 19:19:04 +0700

    add(function): uploading presto backend file, with verif before

---
+++ config/functions/updatePrestoUrl.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+. $FUNCTIONS_CONF/getNgrokUrlFromPresto.sh
+
+function updatePrestoUrl() {
+  url=$1
+
+  if [ ! "$url" -o "$url" = "null" ]
+  then
+    echo Can not upload blank url...
+    exit 1
+  fi
+
+  target="http:"
+  replace="https:"
+  url=${url//$target/$replace}
+
+  distant=$(getNgrokUrlFromPresto)
+  echo "{ \"distant_url\": \""$distant"\", \"current_url\": \""$url"\" }" | jq
+
+  if [ "$distant" = "$url" ]
+  then
+      echo No need to update
+      exit 0
+  fi
+
+  echo Upload process start!
+  echo Moving to ~/dev/presto.vn/
+  cd ~/dev/presto.vn/
+  echo Erase old backend.json file
+  rm -rf ~/dev/presto.vn/backend.json
+  echo "Write new json: "
+  echo "{ \"url\": \""$url"\" }" | jq
+  echo "{ \"url\": \""$url"\" }" >> ~/dev/presto.vn/backend.json
+  echo "Send file to FTP server.."
+  sh ~/dev/presto.vn/updateBackendUrl.sh
+  echo Back to initial directory..
+  cd -
+  echo ""
+  distant2=$(getNgrokUrlFromPresto)
+    if [ "$distant2" = "$url" ]
+  then
+      echo "### Successfully updated"
+      exit 0
+  else
+      echo "### Update failed, distant is: $distant2"
+      exit 1
+  fi
+}
