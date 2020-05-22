a44102439c3c89e07ea303aeee907e499cf403d6
Michael Linhardt <linhardt.michael@gmail.com>
2020-05-22 15:55:30 +0700

    add(export): android sdk path

--- config/zsh/exports.zsh
+++ config/zsh/exports.zsh
@@ -27,3 +27,11 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# ANDROID SDK
+
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
