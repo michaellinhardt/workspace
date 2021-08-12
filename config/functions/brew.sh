a2abcb836241a559461bce406552a24f475408e2
Michael Linhardt <linhardt.michael@gmail.com>
2021-08-12 16:53:34 +0700

    Update cask install function

--- config/functions/brew.sh
+++ config/functions/brew.sh
@@ -82,7 +82,7 @@ function caskInstall () {
 	fi
 
 	echo $CW8"installing $1..."$CWH
-	brew cask install $2 $3
+	brew install --cask $2 $3
 	if [[ $? != 0 ]] ; then
 		echo $CKO"$1 install fail"$CWH
 	else
