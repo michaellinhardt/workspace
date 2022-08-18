d985a3a00d4ee7f0ae7236b0b500ae6de3089504
Michael Linhardt <linhardt.michael@gmail.com>
2022-08-18 15:00:10 +0700

    save

--- config/vsc/vsc_install.sh
+++ config/vsc/vsc_install.sh
@@ -10,4 +10,13 @@ code --install-extension eg2.vscode-npm-script
 code --install-extension christian-kohler.npm-intellisense
 code --install-extension christian-kohler.path-intellisense
 code --install-extension visualstudioexptteam.vscodeintellicode
-# vscInstallExtension "ms-vsliveshare.vsliveshare"
+
+code --install-extension aaron-bond.better-comments
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension eamodio.gitlens
+code --install-extension zignd.html-css-class-completion
+code --install-extension xabikos.javascriptsnippets
+code --install-extension xdebug.php-debug
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension jasonnutter.search-node-modules
+code --install-extension tabnine.tabnine-vscode
