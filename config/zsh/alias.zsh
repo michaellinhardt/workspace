bc3672a9bcd780d6360dd64123774cd515102bd4
Michael Linhardt <linhardt.michael@gmail.com>
2025-03-27 22:27:21 +0700

    adding some ssh commands

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -40,7 +40,77 @@ alias install-npm="sh $NPM_CONF/npm_install.sh"
 alias install-vsc="sh $VSC_CONF/vsc_install.sh"
 alias install-all="sh $BREW_CONF/brew_install.sh && sh $NPM_CONF/npm_install.sh && sh $VSC_CONF/vsc_install.sh"
 
-ssh_dktvn_orders () { ssh -i ~/keys/2024.01.30-DKTVNOrders.pem ubuntu@ec2-52-221-216-32.ap-southeast-1.compute.amazonaws.com }
+ssh_lightsail_restart () {
+  INSTANCE_NAME="dktvnwordpress"
+
+  echo "aws lightsail stop-instance --instance-name $INSTANCE_NAME > /dev/null"
+  aws lightsail stop-instance --instance-name $INSTANCE_NAME > /dev/null
+  
+  # Wait until the instance is stopped
+  while state=$(aws lightsail get-instance-state --instance-name $INSTANCE_NAME --query "state.name" --output text); [ "$state" != "stopped" ]; do
+    echo "Waiting for instance to [stop]. Current state: $state"
+    sleep 5
+  done
+  
+  echo "aws lightsail start-instance --instance-name $INSTANCE_NAME > /dev/null"
+  aws lightsail start-instance --instance-name $INSTANCE_NAME > /dev/null
+
+  # Wait until the instance is running
+  while state=$(aws lightsail get-instance-state --instance-name $INSTANCE_NAME --query "state.name" --output text); [ "$state" != "running" ]; do
+    echo "Waiting for instance to [start]. Current state: $state"
+    sleep 5
+  done
+  
+  echo "Instance is running."
+}
+
+ssh_dktvnblog_restart () {
+  echo "aws ec2 stop-instances --instance-ids i-0a98eb3189888fc95 > /dev/null"
+  aws ec2 stop-instances --instance-ids i-0a98eb3189888fc95 > /dev/null
+  
+  # Wait until the instance is stopped
+  while state=$(aws ec2 describe-instances --instance-ids i-0a98eb3189888fc95 --query "Reservations[*].Instances[*].State.Name" --output text); [ "$state" != "stopped" ]; do
+    echo "Waiting for instance to [stop]. Current state: $state"
+    sleep 5
+  done
+  
+  echo "aws ec2 start-instances --instance-ids i-0a98eb3189888fc95 > /dev/null"
+  aws ec2 start-instances --instance-ids i-0a98eb3189888fc95 > /dev/null
+
+  # Wait until the instance is running
+  while state=$(aws ec2 describe-instances --instance-ids i-0a98eb3189888fc95 --query "Reservations[*].Instances[*].State.Name" --output text); [ "$state" != "running" ]; do
+    echo "Waiting for instance to [start]. Current state: $state"
+    sleep 5
+  done
+  
+  echo "Instance is running."
+}
+
+ssh_dktvnorders_restart () {
+  echo "aws ec2 stop-instances --instance-ids i-0555cc0f0c5e3fffa > /dev/null"
+  aws ec2 stop-instances --instance-ids i-0555cc0f0c5e3fffa > /dev/null
+  
+  # Wait until the instance is stopped
+  while state=$(aws ec2 describe-instances --instance-ids i-0555cc0f0c5e3fffa --query "Reservations[*].Instances[*].State.Name" --output text); [ "$state" != "stopped" ]; do
+    echo "Waiting for instance to [stop]. Current state: $state"
+    sleep 5
+  done
+  
+  echo "aws ec2 start-instances --instance-ids i-0555cc0f0c5e3fffa > /dev/null"
+  aws ec2 start-instances --instance-ids i-0555cc0f0c5e3fffa > /dev/null
+
+  # Wait until the instance is running
+  while state=$(aws ec2 describe-instances --instance-ids i-0555cc0f0c5e3fffa --query "Reservations[*].Instances[*].State.Name" --output text); [ "$state" != "running" ]; do
+    echo "Waiting for instance to [start]. Current state: $state"
+    sleep 5
+  done
+  
+  echo "Instance is running."
+}
+
+ssh_dktvnorders () { ssh -i ~/keys/2024.01.30-DKTVNOrders.pem ubuntu@ec2-47-128-207-99.ap-southeast-1.compute.amazonaws.com }
+ssh_dktvnblog () { ssh -i ~/keys/2024.01.30-DKTVNOrders.pem ubuntu@ec2-3-1-0-199.ap-southeast-1.compute.amazonaws.com }
+ssh_lightsail () { ssh -i ~/dev/secret-keys/aws-keys-michael-linhardt-decathlon/lightsail-dktvnwordpress.pem bitnami@dktvnblog.com }
 
 # DKT FOLDERS
 dkt () { cd ~/dev/dkt/$@ }
