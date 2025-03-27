6b3067c6a26c811bd6b2fb88f91d06ee50feffbc
Michael Linhardt <linhardt.michael@gmail.com>
2025-03-27 22:28:17 +0700

    mysql and react native configs

--- config/zsh/exports.zsh
+++ config/zsh/exports.zsh
@@ -18,8 +18,11 @@ ZSH_THEME="gallois"
 export MAIL=linhardt.michael@gmail.com
 export GIT_NAME="Michael Linhardt"
 
+# MYSQL
+export DYLD_LIBRARY_PATH=/opt/homebrew/opt/openssl@1.1/lib:$DYLD_LIBRARY_PATH
+
 # SYSTEM
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/homebrew/bin"
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/homebrew/bin:/opt/homebrew/opt/mysql@8.4/bin:$PATH"
 
 # CASK option to force install similink in /Applications
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
@@ -28,6 +31,15 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export NVM_DIR="$HOME/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# Append nvm and npm binaries to PATH (nvm should be initialized first)
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+export PATH="$NVM_DIR/versions/node/$(nvm current)/bin:/opt/homebrew/opt/mysql@8.0/bin:$PATH"
+
+# MYSQL for compilers
+export LDFLAGS="-L/opt/homebrew/opt/mysql@8.4/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/mysql@8.4/include"
 
 # ANDROID SDK
 
@@ -38,3 +50,6 @@ export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 export GOOGLE_APPLICATION_CREDENTIALS=/home/$USER/dkt/backend/dsi-dosa-pim-data-fetcher/src/config/analytics.config.json
+
+# REACT NATIVE REQS ( pod install related )
+export LANG=en_US.UTF-8
