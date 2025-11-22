49a16d0e98fab2102afde550b34427a5ca4b8e60
Michael Linhardt <linhardt.michael@gmail.com>
2025-11-22 22:34:30 +0700

    checkpoint

--- zsh.alias/customs.zsh
+++ zsh.alias/customs.zsh
@@ -4,5 +4,8 @@ alias wsone_csv="sh $SCRIPTS/customs/wsone_csv.sh"
 checkpoint () {
     cd ~/secondbrain
     gad -A && gco "checkpoint" && gpu
+	cd -
+	cd ~/workspace
+    gad -A && gco "checkpoint" && gpu
     cd -
 }
