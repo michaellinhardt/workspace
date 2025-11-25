170e4ac7bcf5effcbf5860fd609c7f576c8baf0d
Michael Linhardt <linhardt.michael@gmail.com>
2025-11-25 11:34:52 +0700

    upd

--- claude-code/terminal/alias.zsh
+++ claude-code/terminal/alias.zsh
@@ -7,3 +7,5 @@ alias sopai-clean-all="sh $CLAUDE_WORKSPACE_SCRIPTS/sopai-clean-all.sh"
 
 alias gemini-rag-generator="sh $CLAUDE_WORKSPACE_SCRIPTS/gemini-rag-generator.sh"
 
+alias sopai-obsidian-link="sh $CLAUDE_WORKSPACE_SCRIPTS/sopai-obsidian-link.sh"
+
