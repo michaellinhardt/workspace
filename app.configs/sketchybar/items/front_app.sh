aa9bba56408cb705574a041bd50af75c9de55241
Michael Linhardt <linhardt.michael@gmail.com>
2026-01-07 20:47:44 +0700

    checkpoint

--- app.configs/sketchybar/items/front_app.sh
+++ app.configs/sketchybar/items/front_app.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# Register events (kept for potential future use)
-sketchybar --add event window_focus \
-           --add event windows_on_spaces \
-           --add event title_change
+# Front app display - shows current running app name
+# Subscribes to front_app_switched event to update when app focus changes
 
-# Center app name display DISABLED
-# The title item has been removed per user request
+front_app=(
+  icon.drawing=off
+  label.font="$FONT:Bold:14.0"
+  label.color=0xffe05a6d
+  label.padding_left=12
+  label.padding_right=12
+  background.drawing=off
+  script="$PLUGIN_DIR/front_app.sh"
+)
+
+sketchybar --add item front_app left \
+           --set front_app "${front_app[@]}" \
+           --subscribe front_app front_app_switched aerospace_workspace_change
--- app.configs/sketchybar/plugins/front_app.sh
+++ app.configs/sketchybar/plugins/front_app.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-sketchybar --set $NAME label="$INFO"
+
+# Use $INFO from event, or query current app on initial load
+if [ -n "$INFO" ]; then
+  APP_NAME="$INFO"
+else
+  APP_NAME=$(osascript -e 'tell application "System Events" to get name of first application process whose frontmost is true' 2>/dev/null)
+fi
+
+sketchybar --set $NAME label="$APP_NAME"
