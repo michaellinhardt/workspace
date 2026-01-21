1e5ba441b3f4986f72fab6376a0693b52b0bb20c
Michael Linhardt <linhardt.michael@gmail.com>
2026-01-21 16:12:17 +0700

    checkpoint

--- app.configs/sketchybar/items/calendar.sh
+++ app.configs/sketchybar/items/calendar.sh
@@ -5,14 +5,14 @@
 # Time item - rightmost in group
 time_item=(
   icon=󱑎
-  icon.font="$FONT:Normal:13.0"
+  icon.font="$FONT:Normal:15.0"
   icon.color=$PINK
   icon.padding_left=4
   icon.padding_right=4
   label.font="$FONT:Bold:14.0"
   label.color=$PINK
   label.padding_left=4
-  label.padding_right=10
+  label.padding_right=8
   label="$(date '+%H:%M')"
   background.drawing=off
   update_freq=1
@@ -23,11 +23,13 @@ sketchybar --add item time right       \
            --set time "${time_item[@]}"
 
 # Date item - calendar icon with day format 'Wed 21'
+# NOTE: The calendar icon ($CALENDAR) glyph has extra left spacing built into it,
+# so icon.padding_left is kept lower (2) compared to other group edge items (8)
 date_item=(
   icon=$CALENDAR
-  icon.font="$FONT:Normal:11.0"
+  icon.font="$FONT:Normal:15.0"
   icon.color=$PINK
-  icon.padding_left=10
+  icon.padding_left=0
   icon.padding_right=4
   label.font="$FONT:Normal:14.0"
   label.color=$PINK
